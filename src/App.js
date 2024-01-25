ccaadec2b8ae0d70c8dd181abf5062a15cbc1375
Anthony1Nguyen1 <Anthony1Nguyen1@hotmail.com>
2024-01-25 13:55:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import CardList from './components/CardList/cardlist.component';
+import React, { useState, useEffect } from "react";
+import { SearchBar } from "./components/searchbar/searchbar.component";
+import axios from 'axios';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [searchInput, setSearchInput] = useState("")
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios("https://jsonplaceholder.typicode.com/users");
+      setMonsters(response.data);
+    };
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    let filtered = [];
+    if (searchInput === "") {
+      filtered = monsters
+    } else {
+      filtered = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    }
+    setFilteredMonsters(filtered);
+  }, [monsters, searchInput]);
+
+  const handleInput = e => {
+    setSearchInput(e.target.value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Rolodex</h1>
+      <SearchBar
+        placeholder='Search Monster'
+        handleInput={handleInput}
+      />
+
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
 
 export default App;
+
